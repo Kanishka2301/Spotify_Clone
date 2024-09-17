735cc293c65f4a24f5921273c0529ec008dedaad
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-09-17 18:11:24 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import PlayerContextProvider from "./context/PlayerContext.jsx";
-import ThemeProvider from "./context/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <PlayerContextProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <App />
       </PlayerContextProvider>
     </BrowserRouter>
   </React.StrictMode>
